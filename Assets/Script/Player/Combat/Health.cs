c26871df8fc89753dd74da102fd9bd821a106e2f
Jhonattan Ramirez <camicasii@hotmail.com>
2020-05-04 11:34:17 -0400

    Modificar barra de salud usando UI

--- Assets/Script/Player/Combat/Health.cs
+++ Assets/Script/Player/Combat/Health.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 public class Health : MonoBehaviour
 {   
     public UnityEvent onDeath;
     public int healthBase;
     public Transform healthBar;
     //public int healthBar;
+    public Image healthBar_;
 
     private int healthCurrent;
     
@@ -45,10 +47,17 @@ public class Health : MonoBehaviour
     private void DestroyGameObject(){
         Destroy(gameObject);
     }
+    //Utilizando la escala de la imagen
     public void UpdateHealthBar(){
         float var =(float)healthCurrent/healthBase;
         Vector3 scale = new Vector3(var,1,1);
         healthBar.localScale=scale;
+    }
+    //Modificador de barra de salud mas eficiente 
+    //y sin modificaciones de tranformadas que pueden ocacional problemas
+    public void UpdateHealthBar_(){
+        float var =(float)healthCurrent/healthBase;        
+        healthBar_.fillAmount=var;
     }
   
 }
